9056b6a2c8ba9636c8d0cc26c43075e7fb8a347b
Kirill K <krasaviniko@gmail.com>
2020-05-24 11:55:08 +0300

    move modules

--- run_as_module.py
+++ run_as_module.py
@@ -1,6 +1,6 @@
-from api import run
-# from utils import Map
+from linkcheck.api import run
+from linkcheck.utils import Map
 
 if __name__ == '__main__':
-    # options = Map({'configfile': './developrc', 'url': ['https://www.example.com']})
-    run()
+    options = Map({'configfile': './developrc', 'url': ['https://www.example.com']})
+    run(options)
