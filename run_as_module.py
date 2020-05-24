e067e23e079a2aaed3758f0a8d69a34b7d5bda8d
Kirill K <krasaviniko@gmail.com>
2020-05-24 09:26:57 +0300

    run as module OK

--- run_as_module.py
+++ run_as_module.py
@@ -1,6 +1,6 @@
-import linkchecker
-from utils import Map
+from api import run
+# from utils import Map
 
 if __name__ == '__main__':
-    options = Map({'configfile': './developrc', 'url': ['https://www.example.com']})
-    linkchecker.main(options)
+    # options = Map({'configfile': './developrc', 'url': ['https://www.example.com']})
+    run()
